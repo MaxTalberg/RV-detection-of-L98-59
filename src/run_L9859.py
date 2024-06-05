0d34fa5a55305cd78e0023453c85d96171485180
Max Talberg <mt942@cam.ac.uk>
2024-06-05 14:44:57 +0100

    Initialised unit test for loading pickle file

--- src/run_L9859.py
+++ src/run_L9859.py
@@ -1,4 +1,3 @@
-
 # --- Imports
 import warnings
 from contextlib import contextmanager
---
+++ tests/utils_test.py
@@ -0,0 +1,18 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import unittest
+from utils import load_data_from_pickle
+
+class TestUtils(unittest.TestCase):
+    def test_data_loading(self):
+        X = load_data_from_pickle('datasets/cleaned_data_20240531.pickle')
+        assert 'ESPRESSO_pre' in X and 'ESPRESSO_post' in X and 'HARPS' in X, "Dataset keys missing"
+        assert len(X['ESPRESSO_pre']['RV']) > 0, "Data loading issue for ESPRESSO_pre"
+        assert len(X['ESPRESSO_post']['RV']) > 0, "Data loading issue for ESPRESSO_post"
+        assert len(X['HARPS']['RV']) > 0, "Data loading issue for HARPS"
+
+if __name__ == '__main__':
+    unittest.main()
+    
