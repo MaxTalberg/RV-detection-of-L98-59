e6db261f1964121aa07ff86548e1a7c9c54291eb
Max Talberg <mt942@cam.ac.uk>
2024-06-30 23:39:29 +0100

    Updated documentation

--- src/config_loader.py
+++ src/config_loader.py
@@ -2,6 +2,19 @@ import configparser
 
 
 def load_config(config_file):
+    """
+    Load a configuration file using the configparser library.
+
+    Parameters
+    ----------
+    config_file : str
+        The path to the configuration file that needs to be loaded.
+
+    Returns
+    -------
+    configparser.ConfigParser
+        A ConfigParser object containing the configuration settings from the specified file.
+    """
     config = configparser.ConfigParser()
     config.read(config_file)
     return config
